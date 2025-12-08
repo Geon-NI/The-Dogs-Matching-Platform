56b70127016f768b24c96023675b29985fb10431
GeonNI <woo3113061@naver.com>
2025-12-08 18:27:52 +0900

    1차 최종

--- shelter/shelter-info.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", async () => {
-
-    // Lucide 아이콘 렌더링
-    if (window.lucide) {
-        lucide.createIcons();
-    } else {
-        console.error("Lucide가 로드되지 않았습니다.");
-    }
-});
